76ad91071c09834df2f279010379e444b54b9db7
Irfan <m_ghan@encs.concordia.ca>
2017-03-14 14:03:23 +0530

    first

---
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/example/meruguabhishek/myapplication/UserDetails.java
@@ -0,0 +1,11 @@
+package com.example.meruguabhishek.myapplication;
+
+/**
+ * Created by meruguabhishek on 2017-03-13.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
